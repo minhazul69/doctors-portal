fbe9b6a804f819388b736186cd237c78d9b06031
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-05-14 23:38:37 +0600

    login page updae and user validate

--- src/Pages/Form/Login/Login.jsx
+++ src/Pages/Form/Login/Login.jsx
@@ -1,12 +1,88 @@
 import React from "react";
 import GoogleLogin from "../GoogleLogin";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div className="flex items-center justify-center w-screen my-10">
       <div class="card w-96 bg-base-100 shadow-xl">
         <div class="card-body">
           <h2 class="text-center font-bold text-xl">Login</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div class="form-control w-full max-w-xs ">
+              <label class="label">
+                <span class="label-email">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="Email"
+                class="input input-bordered w-full max-w-xs"
+                {...register("email", {
+                  required: {
+                    value: true,
+                    message: "Email Is Required",
+                  },
+                  pattern: {
+                    value: /[a-z0-9]+@.[a-z]{3}/,
+                    message: "Your Email Have Must Be A Special characters",
+                  },
+                })}
+              />
+              <label className="label">
+                {errors.email?.type === "required" && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.email.message}
+                  </span>
+                )}
+                {errors.email?.type === "pattern" && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.email.message}
+                  </span>
+                )}
+              </label>
+            </div>
+            <div class="form-control w-full max-w-xs ">
+              <label class="label">
+                <span class="label-password">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="Password"
+                class="input input-bordered w-full max-w-xs"
+                {...register("password", {
+                  required: {
+                    value: true,
+                    message: "Password Is Required",
+                  },
+                  minLength: {
+                    value: 6,
+                    message: "Password Must Be 6 characters",
+                  },
+                })}
+              />
+              <label className="label">
+                {errors.password?.type === "required" && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.password.message}
+                  </span>
+                )}
+                {errors.password?.type === "minLength" && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.password.message}
+                  </span>
+                )}
+              </label>
+            </div>
+            <input className="btn w-full" type="submit" value="Login" />
+          </form>
           <div class="divider">OR</div>
           <GoogleLogin />
         </div>
