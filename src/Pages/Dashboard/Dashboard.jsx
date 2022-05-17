0e78df53cec3c283100df0b0a070cc11c7a9ae76
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-05-17 11:28:09 +0600

    view my appointmnet

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -6,20 +6,17 @@ const Dashboard = () => {
   return (
     <div className="lg:px-12">
       <div class="drawer drawer-mobile">
-        <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-        <div class="drawer-content flex flex-col items-center justify-center">
+        <input id="dashboard-dropdown" type="checkbox" class="drawer-toggle" />
+        <div class="drawer-content flex flex-col ">
           {/* <!-- Page content here --> */}
+          <h2 className="text-3xl font-bold text-purple-700 text-center my-10">
+            Welcome To Dashboard
+          </h2>
           <Outlet />
-          <label
-            for="my-drawer-2"
-            class="btn btn-primary drawer-button lg:hidden"
-          >
-            Open drawer
-          </label>
         </div>
         <div class="drawer-side">
-          <label for="my-drawer-2" class="drawer-overlay"></label>
-          <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+          <label for="dashboard-dropdown" class="drawer-overlay"></label>
+          <ul class="menu p-4 overflow-y-auto w-50 bg-base-100 text-base-content">
             {/* <!-- Sidebar content here --> */}
             <li>
               <CustomLink to="/dashboard">My Appointment</CustomLink>
--- src/Pages/Dashboard/MyAppointment/MyAppointment.jsx
+++ src/Pages/Dashboard/MyAppointment/MyAppointment.jsx
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
 
 const MyAppointment = () => {
+  const [appointments, setAppointments] = useState([]);
+  const [user] = useAuthState(auth);
+  useEffect(() => {
+    if (user) {
+      fetch(`http://localhost:5000/booking?patient=${user?.email}`)
+        .then((res) => res.json())
+        .then((data) => setAppointments(data));
+    }
+  }, [user]);
   return (
     <div>
-      <h2>This Is My Appointment</h2>
+      {appointments.length === 0 ? (
+        <h1 className="text-3xl text-red-700 text-center font-bold my-10">
+          You Have No Appointment At This Time
+        </h1>
+      ) : (
+        <div class="overflow-x-auto">
+          <table class="table w-full">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Date</th>
+                <th>Time</th>
+                <th>Treatment</th>
+              </tr>
+            </thead>
+            {appointments.map((appointment) => (
+              <tbody>
+                <tr>
+                  <th>1</th>
+                  <td>{appointment?.patientName}</td>
+                  <td>{appointment.date}</td>
+                  <td>{appointment.option}</td>
+                  <td>{appointment.treatment}</td>
+                </tr>
+              </tbody>
+            ))}
+          </table>
+        </div>
+      )}
     </div>
   );
 };
